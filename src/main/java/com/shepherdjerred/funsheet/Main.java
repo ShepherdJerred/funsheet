31a12e9e2cbdbe818b47936d079c6bdb1f6443ee
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-06-29 00:10:22 +0300

    Initial commit

---
+++ src/main/java/com/shepherdjerred/funsheet/Main.java
@@ -0,0 +1,13 @@
+package com.shepherdjerred.funsheet;
+
+import static spark.Spark.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+        port(8080);
+        get("/", ((request, response) -> "funsheet"));
+    }
+
+}
+
