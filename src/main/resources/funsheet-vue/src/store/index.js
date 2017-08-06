eb714a1ef82454f337ceb5cb3fd6643421051706
Jerred Shepherd <ShepherdJerred@gmail.com>
2017-08-05 21:07:13 -0400

    Split Vuex store into modules

--- src/main/resources/funsheet-vue/src/store/index.js
+++ src/main/resources/funsheet-vue/src/store/index.js
@@ -2,58 +2,19 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import VueResource from 'vue-resource';
 
+import Activities from './modules/activities';
+import Locations from './modules/locations';
+import Types from './modules/types';
+import Tags from './modules/tags';
+
 Vue.use(Vuex);
 Vue.use(VueResource);
 
 export default new Vuex.Store({
-  state: {
-    activities: [],
-    tags: [],
-    types: [],
-    locations: []
-  },
-  mutations: {
-    setActivities (state, value) {
-      state.activities = value;
-    },
-    setTags (state, value) {
-      state.tags = value;
-    },
-    setTypes (state, value) {
-      state.types = value;
-    },
-    setLocations (state, value) {
-      state.locations = value;
-    }
-  },
-  actions: {
-    getActivities (context) {
-      Vue.http.get('/api/activities').then(response => {
-        context.commit('setActivities', response.body);
-      }, response => {
-        console.log(response.body);
-      });
-    },
-    getTags (context) {
-      Vue.http.get('/api/tags').then(response => {
-        context.commit('setTags', response.body);
-      }, response => {
-        console.log(response.body);
-      });
-    },
-    getTypes (context) {
-      Vue.http.get('/api/types').then(response => {
-        context.commit('setTypes', response.body);
-      }, response => {
-        console.log(response.body);
-      });
-    },
-    getLocations (context) {
-      Vue.http.get('/api/locations').then(response => {
-        context.commit('setLocations', response.body);
-      }, response => {
-        console.log(response.body);
-      });
-    }
+  modules: {
+    Activities,
+    Locations,
+    Types,
+    Tags
   }
 });
